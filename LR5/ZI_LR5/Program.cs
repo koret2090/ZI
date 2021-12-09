e5e5b7e87fbc464250bf0d3b5c822d5aeb8b6950
Suslikov Daniil <danyvs@mail.ru>
2021-12-09 16:54:58 +0300

    lr5

---
+++ LR5/ZI_LR5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ZI_LR5
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sign = new Sign("SHA256");
+
+            string dataFilename = "text.txt";
+            string signFilename = "signature.txt";
+
+            //sign.DoSign(dataFilename, signFilename);
+
+            
+            if (sign.CheckSign(dataFilename, signFilename))
+                Console.WriteLine("OK");
+            else
+                Console.WriteLine("Fail");
+            Console.Read();
+            
+        }
+    }
+}
---
+++ LR5/ZI_LR5/Sign.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace ZI_LR5
+{
+    class Sign
+    {
+        HashAlgorithm hashAlg;
+
+        RSACryptoServiceProvider cryptoProvider;
+        RSAPKCS1SignatureDeformatter deformatter;
+        RSAPKCS1SignatureFormatter formatter;
+
+        public Sign(string hashAlgName)
+        {
+            hashAlg = HashAlgorithm.Create(hashAlgName);
+
+            cryptoProvider = new RSACryptoServiceProvider(); // создаются ключи здесь
+            formatter = new RSAPKCS1SignatureFormatter(cryptoProvider);
+            deformatter = new RSAPKCS1SignatureDeformatter(cryptoProvider);
+
+            formatter.SetHashAlgorithm(hashAlgName);
+            deformatter.SetHashAlgorithm(hashAlgName);
+        }
+
+        public void DoSign(string filenameSource, string filenameSignature)
+        {
+            FileStream reader = new FileStream(filenameSource, FileMode.Open);
+            BinaryWriter writer = new BinaryWriter(new FileStream(filenameSignature, FileMode.Create));
+            
+            // рассказывам всем о своей подписи (закрытый ключ RSA - открытый для всех для подписи)
+            var key = cryptoProvider.ExportCspBlob(true);
+            writer.Write(key.Length);
+            writer.Write(key);
+
+            var hash = hashAlg.ComputeHash(reader);
+            var signed_hash = formatter.CreateSignature(hash);
+            int hash_len = signed_hash.Length;
+
+            writer.Write(hash_len);
+            writer.Write(signed_hash);
+            
+
+            reader.Close();
+            writer.Close();
+        }
+
+        public bool CheckSign(string filename, string signatureFilename)
+        {
+            bool isCorrect = false;
+            BinaryReader signatureReader = new BinaryReader(new FileStream(signatureFilename, FileMode.Open));
+            FileStream reader = new FileStream(filename, FileMode.Open);
+                
+            var key = signatureReader.ReadBytes(signatureReader.ReadInt32());
+            cryptoProvider.ImportCspBlob(key);
+
+            var hash_len = signatureReader.ReadInt32();
+            var signed_hash = signatureReader.ReadBytes(hash_len);
+
+            var hash = hashAlg.ComputeHash(reader);
+
+            isCorrect = deformatter.VerifySignature(hash, signed_hash);
+
+            signatureReader.Close();
+            reader.Close();
+
+            return isCorrect;
+        }
+    }
+}
