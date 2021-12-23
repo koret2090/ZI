7f926034f129cc9480207213e4dad62ed3e0edad
Suslikov Daniil <danyvs@mail.ru>
2021-12-23 22:41:03 +0300

    haffman

---
+++ LR6/Huffman/Huffman/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Huffman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Compress or decompress? (c/d)");
+            string method = Console.ReadLine();
+
+            if (method == "c")
+            {
+                var data = File.ReadAllText("test.txt");
+                var tree = new Tree(data);
+                tree.Build();
+                var encoded = tree.Encode(data);
+
+                int bytes = encoded.Count / 8;
+                if ((encoded.Count % 8) != 0) bytes++;
+                byte[] arr = new byte[bytes + 1];
+                int bitIndex = 0, byteIndex = 0;
+                for (int i = 0; i < encoded.Count; i++)
+                {
+                    if (encoded[i])
+                    {
+                        arr[byteIndex] |= (byte)(((byte)1) << bitIndex);
+                    }
+                    bitIndex++;
+                    if (bitIndex == 8)
+                    {
+                        bitIndex = 0;
+                        byteIndex++;
+                    }
+                }
+
+                // trash bits amount
+                arr[bytes] = Convert.ToByte(8 - (encoded.Count % 8) + 8);                
+
+                File.WriteAllBytes("en.txt", arr);
+                File.WriteAllText("fr.txt", JsonSerializer.Serialize(tree.Frequencies));
+            }
+            else if (method == "d")
+            {
+                var fr = JsonSerializer.Deserialize<Dictionary<char, int>>(File.ReadAllText("fr.txt"));
+                var encoded = File.ReadAllBytes("en.txt");
+                var bits = new BitArray(encoded);
+                int trashBits = Convert.ToInt32(encoded[encoded.Length - 1]);
+                var bools = new List<bool>();
+
+                for (var i = 0; i < bits.Length - trashBits; i++)
+                {
+                    bools.Add((bool)bits[i]);
+                }
+
+                var tree = new Tree(fr);
+                tree.Build();
+                var decoded = tree.Decode(bools);
+                Console.WriteLine("Decoded: " + decoded);
+            }
+        }
+    }
+}
---
+++ LR6/Huffman/Huffman/Tree.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Huffman
+{
+    public class Tree
+    {
+        private List<Node> nodes = new List<Node>();
+        public Node Root { get; set; }
+        public Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+
+        public Tree(string source)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Frequencies.ContainsKey(source[i]))
+                {
+                    Frequencies.Add(source[i], 0);
+                }
+
+                Frequencies[source[i]]++;
+            }
+        }
+
+        public Tree(Dictionary<char, int> frequencies)
+        {
+            Frequencies = frequencies;
+        }
+
+        public void Build()
+        {
+            foreach (KeyValuePair<char, int> symbol in Frequencies)
+            {
+                nodes.Add(new Node() { Symbol = symbol.Key, Frequency = symbol.Value });
+            }
+
+            while (nodes.Count > 1)
+            {
+                List<Node> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList<Node>();
+
+                if (orderedNodes.Count > 1)
+                {
+                    // Take first two items
+                    List<Node> taken = orderedNodes.Take(2).ToList<Node>();
+
+                    // Create a parent node by combining the frequencies
+                    Node parent = new Node()
+                    {
+                        Symbol = '*',
+                        Frequency = taken[0].Frequency + taken[1].Frequency,
+                        Left = taken[0],
+                        Right = taken[1]
+                    };
+
+                    nodes.Remove(taken[0]);
+                    nodes.Remove(taken[1]);
+                    nodes.Add(parent);
+                }
+
+                this.Root = nodes.FirstOrDefault();
+            }
+
+        }
+
+        public List<bool> Encode(string source)
+        {
+            List<bool> encodedSource = new List<bool>();
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                List<bool> encodedSymbol = this.Root.Traverse(source[i], new List<bool>());
+                encodedSource.AddRange(encodedSymbol);
+            }
+            return encodedSource;
+        }
+
+        public string Decode(List<bool> bits)
+        {
+            Node current = this.Root;
+            string decoded = "";
+
+            foreach (var bit in bits)
+            {
+                if (bit)
+                {
+                    if (current.Right != null)
+                    {
+                        current = current.Right;
+                    }
+                }
+                else
+                {
+                    if (current.Left != null)
+                    {
+                        current = current.Left;
+                    }
+                }
+
+                if (current.Left == null && current.Right == null)
+                {
+                    decoded += current.Symbol;
+                    current = this.Root;
+                }
+            }
+
+            return decoded;
+        }
+    }
+}
+
